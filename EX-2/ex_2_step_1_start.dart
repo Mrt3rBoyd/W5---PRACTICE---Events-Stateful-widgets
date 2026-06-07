e00b861309015c232dfdc33cca68d6c2c2b84b18
Lor Sophy <lsophy065@gmail.com>
2026-06-07 21:23:24 +0700

    Add files via upload

---
+++ EX-2/ex_2_step_1_start.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.blue,
+          title: const Text("Favorite cards"),
+        ),
+        body: Column(
+          children: [
+            Container(
+                // YOUR CODE
+                )
+          ],
+        ),
+      ),
+    ));
+
+
+// import 'package:flutter/material.dart';
+// void main() => runApp(
+//   MaterialApp(
+//     home: Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue,
+//         title: const Text("Favorite cards"),
+//       ),
+
+
+//       body: Column(
+//         children: [
+//           Container(
+//             decoration: BoxDecoration(),
+//             padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+//             child: Row(
+//               children: [
+              
+                
+//                 const Expanded(
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text('Title', style: TextStyle(color: Colors.blue)),
+//                       SizedBox(height: 10.0),
+//                       Text("Description"),
+//                     ],
+//                   ),
+//                 ),
+//                 IconButton(
+//                   onPressed: () => {},
+//                   icon: const Icon(Icons.favorite, color: Colors.red),
+//                 ),
+  
+//               ],
+
+//             ),
+//           ),
+//         ],
+//       ),
+//     ),
+//   ),
+// );
+
+
+
+// part 3
+// import 'package:flutter/material.dart';
+// void main() => runApp(
+//   MaterialApp(
+//     home: Scaffold(
+//       backgroundColor: Colors.white,
+//       appBar: AppBar(
+//         backgroundColor: Colors.blue,
+//         title: const Text("Favorite cards"),
+//       ),
+
+
+//       body: Column(
+//         children: [
+//           Container(
+//             decoration: BoxDecoration(),
+//             padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+//             child: Row(
+//               children: [
+//                 const Expanded(
+//                   child: Column(
+//                     crossAxisAlignment: CrossAxisAlignment.start,
+//                     children: [
+//                       Text('Title', style: TextStyle(color: Colors.blue)),
+//                       SizedBox(height: 10.0),
+//                       Text("Description"),
+
+//                       Text('Title', style: TextStyle(color: Colors.blue)),
+//                       SizedBox(height: 10.0),
+//                       Text("Description"),
+
+//                       Text('Title', style: TextStyle(color: Colors.blue)),
+//                       SizedBox(height: 10.0),
+//                       Text("Description"),
+//                     ],
+//                   ),
+//                 ),
+                
+//                 Column(
+//                   children: [
+//                     IconButton(
+//                       onPressed: () => {},
+//                       icon: const Icon(Icons.favorite, color: Colors.red),
+//                     ),
+                    
+                    
+//                     IconButton(
+//                       onPressed: () => {},
+//                       icon: const Icon(Icons.favorite, color: Colors.red),
+//                     ),
+//                     IconButton(
+//                       onPressed: () => {},
+//                       icon: const Icon(Icons.favorite, color: Colors.red),
+//                     ),
+//                   ],
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     ),
+//   ),
+// );
+
